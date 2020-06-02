143ade8291d71a503c8ddb9a2c31da904f5bfa02
Kfir-G <kfirg15@gmail.com>
2020-06-02 17:25:18 +0300

    Initial commit

---
+++ Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_6
+{
+    class Book
+    {
+        //-----data fields-----
+        private string name;
+        private int pages;      // if its negative->100
+        private string author;
+
+        //-----properties----
+        public string Name { get => name; set => name = value; }
+        public int Pages { get => pages; set => pages = value; }
+        public string Author { get => author; set => author = value; }
+
+        //-----methods----
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# HW_6
+ include: abstruct classes & interface
